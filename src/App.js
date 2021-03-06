15603c18a1b3f5e3e2ad2bff7ea0559081c96dd4
Fraijo17 <abrahamfraijo17@gmail.com>
2021-03-06 12:55:46 -0700

    0

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import About from "./component/About";
 import SinglePost from "./component/SinglePost";
 import Post from "./component/Post";
 import Project from "./component/Project";
-
+import NavBar from "./component/NavBar";
 
 
 function App() {
   return (
     <BrowserRouter>
+    <NavBar/>
     <Switch>
       <Route component={Home} path='/' />
       <Route component={About} path='/about' />
--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,5 +1,25 @@
 import React from "react"
+import { NavLink} from "react-router-dom";
 
 export default function NavBar() {
-    return <h1>NavBar Page!</h1>
+    return (
+        <header>
+            <div>
+                <nav>
+                    <NavLink to="/" exact>
+                        Abraham
+                    </NavLink>
+                    <NavLink to="../component/Post.js">
+                        Blog Posts
+                    </NavLink>
+                    <NavLink to="../component/Project.js">
+                        Projects
+                    </NavLink>
+                    <NavLink to="../component/About.js">
+                        About me!
+                    </NavLink>
+                </nav>
+            </div>
+        </header>
+    )
 }
