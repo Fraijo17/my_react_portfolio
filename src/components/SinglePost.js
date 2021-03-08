fd73d89f7435112afb65571d55d9db1161b3dcb9
Fraijo17 <abrahamfraijo17@gmail.com>
2021-03-08 13:06:04 -0700

    updating more files

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -68,7 +68,7 @@ export default function SinglePost() {
         <div className="px-16 lg:px-48 py-12 lg:py-20 prose lg:prose-xl max-w-full">
           <BlockContent
             blocks={singlePost.body}
-            projectId="uj2q795a"
+            projectId="w6nw3cku"
             dataset="production"
           />
         </div>
